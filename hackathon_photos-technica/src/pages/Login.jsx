6c9a84a34c4b1ce0e07a8791eac4f84543458cf7
Averie Ahn <averieahn@gmail.com>
2025-07-30 16:52:49 -0400

    made login page and added css. postcreation is still wip (only added basic elements)

---
+++ hackathon_photos-technica/src/pages/Login.jsx
@@ -0,0 +1,17 @@
+function LoginPage() {
+    return(
+        <>
+        <div className="container">
+            <img className="logo" src={technicaPhoto} alt="Technica Logo"></img> 
+            <br></br>
+            <br></br>
+            <button className="SignInButton" id="LoginButton"> Log in</button>
+            <br></br>
+            <br></br>
+            <button className="SignInButton" id="signUpButton"> Sign up</button>
+        </div>
+        </>
+    );
+}
+
+export default LoginPage
---
+++ hackathon_photos-technica/src/pages/UploadPost.jsx
@@ -0,0 +1,12 @@
+const postCollection=[];
+
+
+function PostCreation () {
+    return(
+        <>
+    <input type="text" defaultValue="Enter title here"></input>
+    <input value="text" defaultValue="Enter description here"></input>
+    </>
+    );
+}
+export default PostCreation
