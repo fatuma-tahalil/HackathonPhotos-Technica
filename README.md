f0dd7f2e2b02bbd17a7b4474edd2800d95bb1455
Averie Ahn <averieahn@gmail.com>
2025-08-01 18:31:39 -0400

    made css for upload post

--- README.md
+++
@@ -1,2 +0,0 @@
-# Technica-Stories
-Technica workshop
--- hackathon_photos-technica/src/pages/UploadPost.jsx
+++ hackathon_photos-technica/src/pages/UploadPost.jsx
@@ -1,12 +1,18 @@
+import './UploadPost.css'
 const postCollection=[];
 
 
 function PostCreation () {
     return(
         <>
-        <label for="titleInput" input="text">Enter title:</label>
-    <input type="text" id="titleInput" value="Enter title here"></input>
-    <input type="text" value="Enter description here"></input>
+        <div className='container'>
+            <label for="titleInput" input="text">Enter title:</label>
+            <input type="text" id="titleInput" value="Enter title here"></input>
+            <input type="image" value={"file upload placeholder"}/>
+            <input type="text" id="descBox" value="Enter description here"></input>
+            <input type="text" value="Enter tags here"></input>
+            <button type="submit" id="uploadBtn">Upload</button>
+        </div>
     </>
     );
 }
