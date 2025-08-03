f8cdfbc7ad0a56dfc43b6cd571fb7ef375a9568e
Averie Ahn <averieahn@gmail.com>
2025-08-03 18:55:56 -0400

    added checkbox and finished css for uploadPost

--- hackathon_photos-technica/src/pages/Login.jsx
+++ hackathon_photos-technica/src/pages/Login.jsx
@@ -1,31 +1,14 @@
 import './UploadPost.css';
-import LoginButton from "./components/login.jsx";
-import LogoutButton from "./components/logout.jsx"
-import { useEffect } from 'react';
-import { gapi } from 'gapi-script';
 
-const clientId = 481280737880-rei7cg71h4bpor4st11s0o5996fmkn4r.apps.googleusercontent.com;
 
 function LoginPage() {
-
-    useEffect(()=> {
-        function start() {
-            gapi.client.init({
-                clientId: clientId,
-                scope:""
-            })
-        };
-
-        gapi.load('client:auth2', start);
-    })
     return(
         <>
         <div className="container">
             <img className="logo" src={technicaPhoto} alt="Technica Logo"></img> 
             <br></br>
             <br></br>
-            <LoginButton/>
-            <LogoutButton/>
+            <button id="button"></button>
             <br></br>
             <br></br>
             <button className="SignInButton" id="signUpButton"> Sign up</button>
--- hackathon_photos-technica/src/pages/UploadPost.jsx
+++ hackathon_photos-technica/src/pages/UploadPost.jsx
@@ -1,4 +1,5 @@
 import './UploadPost.css'
+import MultipleSelectCheckmarks from './multipleCheckbox.tsx'
 const postCollection=[];
 
 
@@ -6,12 +7,22 @@ function PostCreation () {
     return(
         <>
         <div className='container'>
-            <label for="titleInput" input="text">Enter title:</label>
-            <input type="text" id="titleInput" value="Enter title here"></input>
-            <input type="image" value={"file upload placeholder"}/>
-            <input type="text" id="descBox" value="Enter description here"></input>
-            <input type="text" value="Enter tags here"></input>
+            <label htmlFor="titleInput" id="titleLabel">Enter title:</label>
+            <input type="text" id="titleInput" defaultValue="Enter title here"></input>
+        <form className="box" method="post" action="" enctype="multipart/form-data">
+            <div id="drop-zone">
+            <div className="drop-zone-caption">Drag & Drop Files Here</div>
+                <span className="btn btn-primary btn-file" style="position: relative">
+                <span>Choose files</span>
+                <input type="file"id="drop-zone-file" name="files[]" multiple/>
+            </span>
+             </div>
+        </form>
+            <input type="image" defaultValue={"file upload placeholder"}/>
+            <input type="text" id="descBox" defaultValue="Enter description here"></input>
+            <input type="text" defaultValue="Enter tags here"></input>
             <button type="submit" id="uploadBtn">Upload</button>
+            <MultipleSelectCheckmarks/>
         </div>
     </>
     );
