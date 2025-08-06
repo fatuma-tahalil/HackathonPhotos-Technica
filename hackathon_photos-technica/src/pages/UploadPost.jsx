280bf800bd5cf7cdde4d85192896594b9ad326b3
FatumaT <regretmakingthis@gmail.com>
2025-08-05 22:52:17 -0400

    Updated upload photo page
    - Upload button
    - Tags
    - Upload file features

--- hackathon_photos-technica/src/pages/UploadPost.jsx
+++ hackathon_photos-technica/src/pages/UploadPost.jsx
@@ -1,13 +1,30 @@
-import '../css/App.css';
-// import '../css/UploadPost.css';
+import { transform } from 'typescript';
+import MultipleSelectCheckmarks from '../components/multipleCheckbox.tsx';
+import '../css/UploadPost.css';
+const postCollection=[];
 
-const UploadPost = () => {
-    return(
-        <div className='page'>
-            <label for="titleInput" input="text">Enter title:</label>
-            <input type="text" id="titleInput" value="Enter title here"></input>
-            <input type="text" value="Enter description here"></input>
+
+
+function PostCreation () {
+    return (
+        <div className='container page'>
+            <label htmlFor="titleInput" id="titleLabel">Enter title:</label>
+            <input type="text" id="titleInput" defaultValue="Enter title here"></input>
+            <form className="box" method="post" action="" encType="multipart/form-data">
+                <div id="drop-zone">
+                <div className="drop-zone-caption">Drag & Drop Files Here</div>
+                    <span className="btn btn-primary btn-file" style= {{position: "relative"}}>
+                    <span>Choose files</span>
+                    <input type="file"id="drop-zone-file" name="files[]" multiple/>
+                </span>
+                </div>
+            </form>
+            <input type="image" defaultValue={"file upload placeholder"} alt="upload file"/>
+            <input type="text" id="descBox" defaultValue="Enter description here"></input>
+            <input type="text" defaultValue="Enter tags here"></input>
+            <button type="submit" id="uploadBtn">Upload</button>
+            <MultipleSelectCheckmarks/>
         </div>
-    )
+    );
 }
-export default UploadPost
+export default PostCreation
