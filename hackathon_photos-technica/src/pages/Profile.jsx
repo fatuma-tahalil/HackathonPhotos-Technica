6b0703c1c7ea82c1d9df4742b40e45c4aac47102
FatumaT <regretmakingthis@gmail.com>
2025-08-08 11:54:09 -0400

    Conditionally renders the user profile if loaded or puts a placeholder if not

--- hackathon_photos-technica/src/pages/Profile.jsx
+++ hackathon_photos-technica/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import icon from '../assets/icon.png';
@@ -7,14 +7,25 @@ import '../css/Profile.css';
 
 // TODO: Add edit image feature: Event handler when button is clicked, ability to change name, read name change and then update user info
 const Profile = ({ user }) => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+
+    useEffect(() => {
+    if (user?.photoURL !== null) {
+        setImageLoaded(true);
+    }
+    }, [user?.photoURL])
+
+
     return (
         <>
             {/* Profile section */}
             <section className="profile-container page">
                 <h1 className="name">{user?.displayName || "loading.."}</h1>
                 <div className="padding-medium"></div>
-                {/* TODO: Add placeholder image while google profile image loads */}
-                <img src={user?.photoURL || icon} className="icon" alt="user icon" />
+
+                {/* Sets profile image to the google profile if it has loaded or to the placeholder image if not*/}
+                {imageLoaded ? (<img src={user.photoURL || icon} className="icon" alt="user icon" />) :(<img src={icon} className="icon" alt="user icon" />)}
+
                 <div className="padding-medium"></div>
                 <button /*onClick={handleEditImage}*/ className="profile-button">
                     <h1>Edit Account</h1>
