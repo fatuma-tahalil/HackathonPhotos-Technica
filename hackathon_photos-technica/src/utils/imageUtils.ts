1b60ad5f606cc3dfe29f1dc19491c313cd3d24f1
FatumaT <regretmakingthis@gmail.com>
2025-08-10 03:23:18 -0400

    Remove image utils. Not needed since we are not using imgur anymore.

--- hackathon_photos-technica/src/utils/imageUtils.ts
+++
@@ -1,9 +0,0 @@
-/* Edits the image URL hosted on imgur to be accessible in this local host repo */
-export function editImageUrl(imageURL) {
-    if (imageURL.contains("imgur")) {
-        return imageURL.replace('imgur.com/' , 'i.imgur.com/') + '.png';
-    }
-    else {
-        return imageUrl;
-    }
-}
