719ec5fb6a6b616db73b9c88e4c50c11f0248f88
FatumaT <regretmakingthis@gmail.com>
2025-08-10 16:19:48 -0400

    Added upload post functionality - Converts storage folder to database folder names because I stupidly didn't name them the same - Uploads images to cloud storage - Uploads images to the database - Removed imagepath because it's either a file object or a storageURL that is defined in uploadImage.ts - We generated user and photo id in uploadImage.ts - We handle file input seperately through states by treating it as an object and not a string (also file input returns a list)

---
+++ hackathon_photos-technica/src/firebase/uploadImage.ts
@@ -0,0 +1,69 @@
+import { storage, auth, db } from './config.ts';
+import { ref, getDownloadURL, uploadBytes } from 'firebase/storage';
+import { doc, setDoc, Timestamp } from "firebase/firestore";
+import type { Photo } from '../types/types.ts';
+import { v4 as uuidv4 } from 'uuid';
+
+
+const storageToDatabaseFolder = {
+    "group-photos": "photoGroup",
+    "hardware-hacks": "photoHardware",
+    "workshops": "photoWorkshop"
+}
+
+export async function uploadImage(imageObject: object, file: File){
+    const tagsInPost = imageObject.tags
+    console.log("TAGS: ", tagsInPost);
+    if (tagsInPost.includes("# hardware-hacks")) {
+        await uploadImageToCloudStorage(imageObject, "hardware-hacks", file);
+    } 
+    if (tagsInPost.includes("# group-photos")) {
+        await uploadImageToCloudStorage(imageObject, "group-photos", file);
+    } 
+    if (tagsInPost.includes("# workshops")) {
+        await uploadImageToCloudStorage(imageObject, "workshops", file);
+    } 
+    else if (tagsInPost.length() == 0) {
+        console.error("No tag was selected");
+    }
+}
+
+async function uploadImageToCloudStorage(imageObject: object, folder: string, file: File) {
+    try{
+        const imageRef = ref(storage, `photos/${folder}/${file.name}`);
+        await uploadBytes(imageRef, file);
+        const imageStorageURL = await getDownloadURL(imageRef); 
+        const databaseFolder = storageToDatabaseFolder[folder];
+        await uploadImageToDatabase(imageObject, databaseFolder, imageStorageURL);
+    } catch (error){
+        console.error(error);
+    }
+
+}
+
+async function uploadImageToDatabase(imageObject: object, folder: string, storageURL: string){
+    
+    const user = auth.currentUser;
+    
+    if (!user) {
+        console.error('User must be logged in to add photos');
+        return;
+    }
+
+    try {
+        const photoData: Photo = {
+            createdAt: Timestamp.now(),
+            description: imageObject.description || null,
+            id: uuidv4(),
+            imagePath: storageURL,
+            title: imageObject.title,
+            userID: user.uid
+        };
+        console.log("photo data:", photoData);
+        // The document id will be the photo id
+        const photoRef = doc(db, folder, photoData.id);
+        await setDoc(photoRef, photoData);
+    } catch (error) {
+        console.error(error);
+    }
+}
--- hackathon_photos-technica/src/pages/UploadPost.jsx
+++ hackathon_photos-technica/src/pages/UploadPost.jsx
@@ -1,75 +1,68 @@
 import "../css/App.css";
-// import "../css/UploadPost.css";
-import MultipleSelectCheckmarks from '../components/multipleCheckbox.tsx';
 import '../css/UploadPost.css';
+import { uploadImage } from '../firebase/uploadImage.ts';
+import MultipleSelectCheckmarks from '../components/multipleCheckbox.tsx';
 import {useState} from "react";
 import {Timestamp} from 'firebase/firestore';
-import { UUID } from "https://unpkg.com/uuidjs@^5";
-import {auth} from '../firebase/config.ts';
-
 
 function PostCreation () {
-
+    const [file, setFile] = useState(null);
+    const [personName, setPersonName] = useState([]);
 
     const [values, setValues] = useState({
+        createdAt: "",
+        description: "",
+        id: "",
+        tags: [],
+        title: "",
+        userID: ""
+    });
+
+    const handleChanges = (e) => {
+        const { name, value } = e.target;    
+        setValues((prev) => ({ ...prev, [name]: value }));
+    };
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    }
+
+    const handleSubmit = async (e) => {
+
+        e.preventDefault();
+        const dataToSubmit = {
+            ...values, 
+            createdAt : Timestamp.now(), 
+            tags: personName, 
+            id: "",
+            userID: ""
+        };
+        await uploadImage(dataToSubmit, file); // Sends the image to be sorted and uploaded to storage and the database
+        setValues({
             createdAt: " ",
             description: " ",
             id: "",
-            imagePath: " ",
-            tags: [],
+            tags: ' ',
             title: " ",
             userID: " "
-});
-
-const [personName, setPersonName] = useState([]);
-
-const [submittedData, setSubmittedData] = useState(null);
-
-const handleChanges = (e) => {
-    const { name, value } = e.target;    
-     setValues((prev) => ({ ...prev, [name]: value }));
-};
-
-const handleSubmit = (e) => {
-
-    e.preventDefault();
-     const timestamp = Timestamp.now();
-     setSubmittedData({ ...values, 
-        createdAt : timestamp, 
-        tags: personName, 
-        id: UUID.generate(),
-        userID: auth});
-
-        console.log(submittedData)
-     setValues({
-        createdAt: " ",
-        description: " ",
-        id: "",
-        imagePath: " ",
-        tags: ' ',
-        title: " ",
-        userID: " "});
-     }
-     
-    
-     
-
+        });
+        setFile(null);
+    }
     
     return (
         <div className='container'>
-        <form onSubmit = {handleSubmit}>
-            <label htmlFor="titleInput" id="titleLabel">Enter title:</label>
-            <input type="text" id="titleInput" placeholder="Enter title here" value={values.title}name="title"
-            onChange = {(e) => handleChanges(e)} required/>
-                    <input type="file" id="drop-zone-file" name="imagePath" multiple 
-                    onChange = {(e) => handleChanges(e)} required/>
-        
-            <label htmlFor="descBox">description</label>
-            <input type="text" id="descBox" placeholder="Enter description here" name="description" value={values.description}
-            onChange = {(e) => handleChanges(e)} required/>
-            <button type="submit" id="uploadBtn">Upload</button>
-            <MultipleSelectCheckmarks personName = {personName} setPersonName = {setPersonName}/>
-        </form>
+            <form onSubmit = {handleSubmit}>
+                <label htmlFor="titleInput" id="titleLabel">Enter title:</label>
+                <input type="text" id="titleInput" placeholder="Enter title here" value={values.title}name="title"
+                onChange = {(e) => handleChanges(e)} required/>
+                <input type="file" id="drop-zone-file" name="imagePath" multiple 
+                onChange = {handleFileChange} required/>
+            
+                <label htmlFor="descBox">description</label>
+                <input type="text" id="descBox" placeholder="Enter description here" name="description" value={values.description}
+                onChange = {(e) => handleChanges(e)} required/>
+                <button type="submit" id="uploadBtn">Upload</button>
+                <MultipleSelectCheckmarks personName = {personName} setPersonName = {setPersonName}/>
+            </form>
         </div>
     );
 }
