18d1aa6e4dad10abe27b9145b50aab66abc22d6e
Averie Ahn <averieahn@gmail.com>
2025-08-10 14:55:54 -0400

    added reset button

--- hackathon_photos-technica/src/pages/UploadPost.jsx
+++ hackathon_photos-technica/src/pages/UploadPost.jsx
@@ -6,19 +6,20 @@ import {useState} from "react";
 import {Timestamp} from 'firebase/firestore';
 import { UUID } from "https://unpkg.com/uuidjs@^5";
 import {auth} from '../firebase/config.ts';
+import React, { useRef } from "react";
 
 
 function PostCreation () {
 
 
     const [values, setValues] = useState({
-            createdAt: " ",
-            description: " ",
+            createdAt: "",
+            description: "",
             id: "",
-            imagePath: " ",
+            imagePath: "",
             tags: [],
-            title: " ",
-            userID: " "
+            title: "",
+            userID: ""
 });
 
 const [personName, setPersonName] = useState([]);
@@ -29,7 +30,20 @@ const handleChanges = (e) => {
     const { name, value } = e.target;    
      setValues((prev) => ({ ...prev, [name]: value }));
 };
+const fileInputRef = useRef(null);
 
+  const handleReset = () => {
+    fileInputRef.current.value = "";
+    setValues({
+        createdAt: "",
+        description: "",
+        id: "",
+        imagePath: null,
+        tags: null,
+        title: "",
+        userID: ""});
+     
+  };
 const handleSubmit = (e) => {
 
     e.preventDefault();
@@ -41,14 +55,15 @@ const handleSubmit = (e) => {
         userID: auth});
 
         console.log(submittedData)
+      
      setValues({
-        createdAt: " ",
-        description: " ",
+        createdAt: "",
+        description: "",
         id: "",
-        imagePath: " ",
-        tags: ' ',
-        title: " ",
-        userID: " "});
+        imagePath: null,
+        tags: null,
+        title: "",
+        userID: ""});
      }
      
     
@@ -61,13 +76,14 @@ const handleSubmit = (e) => {
             <label htmlFor="titleInput" id="titleLabel">Enter title:</label>
             <input type="text" id="titleInput" placeholder="Enter title here" value={values.title}name="title"
             onChange = {(e) => handleChanges(e)} required/>
-                    <input type="file" id="drop-zone-file" name="imagePath" multiple 
+                    <input type="file" id="drop-zone-file" name="imagePath" multiple ref={fileInputRef} 
                     onChange = {(e) => handleChanges(e)} required/>
         
             <label htmlFor="descBox">description</label>
-            <input type="text" id="descBox" placeholder="Enter description here" name="description" value={values.description}
+            <input type="text" id="descBox" placeholder="Enter description here" name="description" value={values.description} 
             onChange = {(e) => handleChanges(e)} required/>
             <button type="submit" id="uploadBtn">Upload</button>
+            <button type="reset" onClick={handleReset} id="resetBtn">Reset</button>
             <MultipleSelectCheckmarks personName = {personName} setPersonName = {setPersonName}/>
         </form>
         </div>
