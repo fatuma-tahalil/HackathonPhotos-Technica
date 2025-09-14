95dc256da16de04f3be58a8b65673dcfe89d62e9
Fatuma Tahalil <regretmakingthis@gmail.com>
2025-09-14 13:23:38 -0400

    Improved profile edit page - Able to view profile picture in editing page - Customized profile edit CSS - Improved fetch file button

--- hackathon_photos-technica/src/components/ProfileEditForm.jsx
+++ hackathon_photos-technica/src/components/ProfileEditForm.jsx
@@ -1,6 +1,9 @@
 
-import  { useState } from "react";
+import { useState, useEffect } from "react";
 import { updateProfile } from '../firebase/updateProfile.ts';
+import { fetchProfilePhoto } from '../firebase/fetchProfile.ts';
+import editIcon from '../assets/editIcon.svg';
+
 import PropTypes from 'prop-types';
 import '../css/App.css';
 import '../css/Profile.css';
@@ -8,7 +11,7 @@ import '../css/Profile.css';
 const ProfileEditForm = ({ onSave }) => {
     const [selectedImage, setSelectedImage] = useState(null);
     const [name, setName] = useState("");
-
+    const [profileImage, setProfileImage] = useState(null); //  We grab the photo from the database
 
     const handleFileChange = (e) => {
         setSelectedImage(e.target.files[0]);
@@ -27,14 +30,33 @@ const ProfileEditForm = ({ onSave }) => {
     }
     //const fileInputRef = useRef(null);*/
 
+    useEffect(() => {
+        const fetchProfile = async() => {
+            try {
+                setProfileImage(await fetchProfilePhoto());
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchProfile();
+    })
+
     return (
         /* Profile section */
         <form className="profile-container page" onSubmit={handleSubmit}autoComplete="off" >
             <label htmlFor="name" type="name">Enter name</label>
-            <input id="name" type="text" value={name} onChange = {handleNameChange}/>
+            <input id="name" type="text" value={name} onChange={handleNameChange} className="profile-name-input" placeholder="Enter your name" />
             <div className="padding-medium"></div>
-            <input type="file" id="drop-zone-file" name="imagePath" onChange={handleFileChange} accept="image/*"/>
+
+            <div className="photo-edit-wrapper">
+                <img src={selectedImage ? URL.createObjectURL(selectedImage) : profileImage} alt="profile preview" className="edit-photo" />
+                <label htmlFor="drop-zone-file" className="edit-photo-icon" title="Edit photo" >
+                    <img src={editIcon} alt="Edit" style={{ width: "1.5rem", height: "1.5rem" }} />
+                </label>
+                <input type="file" className="drop-zone-file" id="drop-zone-file" name="imagePath" onChange={handleFileChange}  accept="image/*" />
+            </div>
             <div className="padding-medium"></div>
+
             <button type="submit" className="profile-button button">
                 <h1>Submit Changes</h1>
             </button>
@@ -45,6 +67,7 @@ const ProfileEditForm = ({ onSave }) => {
 
 ProfileEditForm.propTypes = {
     onSave: PropTypes.func.isRequired,
+    photo: PropTypes.string.isRequired
 };
 
 export default ProfileEditForm;
